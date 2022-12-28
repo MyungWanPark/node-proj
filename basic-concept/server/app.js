a4e44dbbc051908452888519e515e5a62fa67e07
parkmyungwan <mywanpark@gmail.com>
2022-12-28 09:59:17 +0900

    Basic: CreateReadStream and send html

--- basic-concept/server/app.js
+++ basic-concept/server/app.js
@@ -1,9 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.write('hello server!');
-    res.end()
-
+    console.log(req.httpVersion);
+    console.log(req.method);
+    console.log(req.url);
+    res.setHeader('Content-type', 'text/html');
+    if (req.url === '/') {
+        fs.createReadStream('./html/index.html').pipe(res);
+    } else if (req.url === '/course') {
+        fs.createReadStream('./html/courses.html').pipe(res);
+    } else {
+        fs.createReadStream('./html/not-found.html').pipe(res);
+    }
 })
 
 server.listen(8080);
