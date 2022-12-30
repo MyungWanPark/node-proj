2d32b47d7cb7b7a4ef3b3c4a59aa76e27b4fecf1
parkmyungwan <mywanpark@gmail.com>
2022-12-30 10:33:00 +0900

    Basic: Use Postman to create new data

--- basic-concept/server/app.js
+++ basic-concept/server/app.js
@@ -1,18 +1,35 @@
 const http = require('http');
-const fs = require('fs');
+const courses = [
+    {name: 'html'},
+    {name: 'css'},
+    {name: 'js'}
+]
 
 const server = http.createServer((req, res) => {
-    console.log(req.httpVersion);
-    console.log(req.method);
-    console.log(req.url);
-    res.setHeader('Content-type', 'text/html');
-    if (req.url === '/') {
-        fs.createReadStream('./html/index.html').pipe(res);
-    } else if (req.url === '/course') {
-        fs.createReadStream('./html/courses.html').pipe(res);
-    } else {
-        fs.createReadStream('./html/not-found.html').pipe(res);
-    }
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/courses') {
+        if (method === 'GET') {
+            res.writeHead(200, { 'Content-Type':'application/json' });
+            res.end(JSON.stringify(courses))
+
+        } else if (method === 'POST') {
+            const body = [];
+            req.on('data', (chunk) => {
+                console.log(chunk);
+                body.push(chunk);
+            });
+            req.on('end', () => {
+                const bodyStr = Buffer.concat(body).toString();
+                const course = JSON.parse(bodyStr);
+                courses.push(course);
+                console.log(courses);
+                res.writeHead(201);
+                res.end();
+            })
+        }
+    } 
 })
 
 server.listen(8080);
