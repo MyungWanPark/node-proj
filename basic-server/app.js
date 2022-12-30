20cc8ce628d16935f62dc02ec1e3dce4034305a5
parkmyungwan <mywanpark@gmail.com>
2022-12-31 08:56:34 +0900

    Basic: middleware

--- basic-server/app.js
+++ basic-server/app.js
@@ -1,12 +1,47 @@
 import express from 'express';
 const app = express();
 
-app.get('/hello/:id', (req, res, next) => {
-  // console.log(req.url);
-  // console.log(req.headers);
-  console.log(req.params.id);
-  console.log(req.query.query);
-  res.send('hi!');
+app.all('/park', (req, res, next) => {
+  console.log('all');
 });
 
+app.use('/myung', (req, res, next) => {
+  console.log('use');
+});
+
+app.get(
+  '/hello',
+  (req, res, next) => {
+    console.log('first');
+    next();
+  },
+  (req, res, next) => {
+    console.log('first-2');
+    next();
+  }
+);
+
+app.get(
+  '/hello',
+  (req, res, next) => {
+    console.log('second');
+    next();
+  },
+  (req, res, next) => {
+    console.log('second-2');
+    if (true) {
+      return res.send('finished!');
+    }
+    res.send('finished!');
+  }
+);
+
+app.use((req, res, next) => {
+  res.status(404).send('not found ㅜㅜ');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.send('not found @-@');
+});
 app.listen(8080);
