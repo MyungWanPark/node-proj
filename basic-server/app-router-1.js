b6b33736289b9c91d2992b6f39c5b6e55fd1dfc8
parkmyungwan <mywanpark@gmail.com>
2023-01-01 16:27:36 +0900

    Basic: respond to cors

--- basic-server/app-router-1.js
+++ basic-server/app-router-1.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import postRouter from './router/post.js';
 import usersRouter from './router/users.js';
+import cors from 'cors';
+
+/* const option = {
+  dotfiles: 'ignore',
+  maxAge: '1d',
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+    res.set('x-hello', Date.now());
+  },
+}; */
 
 const app = express();
 
 app.use('/post', postRouter);
 app.use('/users', usersRouter);
-
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ['http://127.0.0.1:5500'],
+  })
+);
+app.use(express.static('public'));
 app.listen(8080);
