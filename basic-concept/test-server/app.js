e1c40dad5c913890629dd3a69d99be9c259dcd6c
parkmyungwan <mywanpark@gmail.com>
2023-01-03 16:27:30 +0900

    Refactor: Extract duplicated function

--- basic-concept/test-server/app.js
+++ basic-concept/test-server/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import { body, param, validationResult } from 'express-validator';
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array()[0].msg });
+  }
+  next();
+};
+
 const app = express();
 
 app.use(express.json());
@@ -11,25 +19,19 @@ app.post(
       .isLength({ min: 2 })
       .withMessage('이름은 두 글자 이상이여야 합니다!!'),
     body('email').isEmail().withMessage('이메일을 정확하게 입력해주세요.'),
+    validate,
   ],
   (req, res, next) => {
     console.log(req.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
+
     res.sendStatus(201);
   }
 );
 
 app.get(
   '/:age',
-  param('age').isInt().withMessage('숫자를 입력해 주세요.'),
+  [param('age').isInt().withMessage('숫자를 입력해 주세요.'), validate],
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     res.send('나이 요청이 잘 도착했어요.');
   }
 );
